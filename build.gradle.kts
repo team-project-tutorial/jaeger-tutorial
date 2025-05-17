335f25256332b3861be852b8d45bf3faa215b785
choi-ys <project.log.062@gmail.com>
2025-05-17 19:32:07 +0900

    build: add project build options
    edit build.gradle.kts
    - add allprojects options
      - set dependency repository
      - set project group
    
    - add subprojects options
      - set global plugin/dependencies options
      - defined global build task
    
    edit setting.gradle.kts
    - add auto register multi-module script

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,10 @@
+import org.springframework.boot.gradle.tasks.bundling.BootJar
+
 plugins {
     java
     id("org.springframework.boot") version "3.4.5"
     id("io.spring.dependency-management") version "1.1.7"
 }
-
-group = "io.stackwise"
-version = "0.0.1-SNAPSHOT"
 
 java {
     toolchain {
@@ -13,16 +12,31 @@ java {
     }
 }
 
-repositories {
-    mavenCentral()
+allprojects {
+    group = providers.gradleProperty("PROJECT_GROUP").get()
+
+    repositories {
+        mavenCentral()
+    }
 }
 
-dependencies {
-    implementation("org.springframework.boot:spring-boot-starter")
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+subprojects {
+    apply(plugin = "java")
+    apply(plugin = "io.spring.dependency-management")
+    apply(plugin = "org.springframework.boot")
+
+    dependencies {
+        compileOnly("org.projectlombok:lombok")
+        annotationProcessor("org.projectlombok:lombok")
+
+        testImplementation("org.springframework.boot:spring-boot-starter-test")
+        testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    }
+
+    tasks.withType<BootJar> { archiveFileName.set("${project.name}.jar") }
+    tasks.withType<Jar> { enabled = false }
+    tasks.withType<Test> { useJUnitPlatform() }
 }
 
-tasks.withType<Test> {
-    useJUnitPlatform()
-}
+tasks.withType<BootJar> { enabled = false }
+tasks.withType<Test> { useJUnitPlatform() }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,23 @@
-rootProject.name = "commerce"
+rootProject.name = "jaeger-tutorial"
+
+registerModules(rootDir)
+
+private fun registerModules(directory: File) = directory.walkTopDown()
+    .filter { isNotRootModule(it, directory) && it.isDirectory && existBuildFile(it) }
+    .forEach { includeModule(it) }
+
+private fun isNotRootModule(it: File, directory: File) = it != directory
+private fun existBuildFile(it: File) = File(it, "build.gradle.kts").exists()
+
+private fun includeModule(moduleDir: File) {
+    val relativePath = moduleDir.relativeTo(rootDir).path
+    val moduleName = convertPathToModuleName(relativePath)
+
+    include(moduleName)
+}
+
+private fun convertPathToModuleName(relativePath: String): String {
+    val modulePrefix = ":"
+    val moduleName = relativePath.replace(File.separatorChar, modulePrefix.first())
+    return "$modulePrefix$moduleName"
+}
